8c58aa324192b67502cd587b96319ebf528c8321
mpayen <marlon.payen4@gmail.com>
2024-07-13 11:35:32 +0200

    Deploy

--- BoisTom/settings.py
+++ BoisTom/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Base url to serve media files
 MEDIA_URL = 'website/media/website/'
