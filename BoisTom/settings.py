9a92ea2b8b434200231a45d5143c6249e48815ea
mpayen <marlon.payen4@gmail.com>
2024-07-13 11:50:29 +0200

    Deploy

--- BoisTom/settings.py
+++ BoisTom/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, '/website/media/website/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
