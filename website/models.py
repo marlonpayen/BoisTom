198d65ab3cbcad98fb656e76a159e450dacd013c
mpayen <marlon.payen4@gmail.com>
2024-07-14 14:17:04 +0200

    Deployment

--- website/models.py
+++ website/models.py
@@ -53,7 +53,7 @@ class Lien(models.Model):
 
 
 class Image(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to='media')
     produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
     
     
