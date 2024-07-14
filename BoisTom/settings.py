5f550e04737b6e4a883cd32c0fe8491ed3e9232d
mpayen <marlon.payen4@gmail.com>
2024-07-14 10:20:04 +0200

    D

--- BoisTom/settings.py
+++ BoisTom/settings.py
@@ -161,7 +161,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ########################################### Deployment ###########################################
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
