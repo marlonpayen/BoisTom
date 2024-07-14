66ecadff27c655eaf85c76433741935dcd8fde9a
mpayen <marlon.payen4@gmail.com>
2024-07-14 11:32:46 +0200

    D

--- BoisTom/settings.py
+++ BoisTom/settings.py
@@ -31,7 +31,7 @@ load_dotenv()
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['boistom.onrender.com', '127.0.0.1', 'localhost']
 
 #ALLOWED_HOSTS = []
--- BoisTom/urls.py
+++ BoisTom/urls.py
@@ -38,7 +38,6 @@ urlpatterns = [
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = "website.views.my_custom_page_not_found_view"
 handler500 = "website.views.my_custom_error_view"
