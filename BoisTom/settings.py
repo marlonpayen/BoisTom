f20bf0196663634d8d6e99da123adffa87fb1682
mpayen <marlon.payen4@gmail.com>
2024-07-20 09:09:23 +0200

    D

--- BoisTom/settings.py
+++ BoisTom/settings.py
@@ -157,7 +157,12 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+    STORAGES = {
+        'staticfiles': {
+            'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        },
+    }
     
     SECURE_HSTS_SECONDS = 15780000  # 6 Months as Recommended
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
