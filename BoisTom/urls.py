7d28bd001da024c6863139948c5c1ffee00c526f
mpayen <marlon.payen4@gmail.com>
2024-07-20 10:23:43 +0200

    D

--- BoisTom/urls.py
+++ BoisTom/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve 
 from website import views
 from django.conf.urls.static import static
 from django.core.exceptions import PermissionDenied
@@ -31,6 +32,7 @@ def permission_denied_view(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('', views.index, name='index'),
     path('home', views.index, name='index'),
     path('messagesent', views.send_message, name='form'),
